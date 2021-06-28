53a4646931c30ad7bfd42b0adcf695771694db19
gabri <gabriel.lopes2@universo.univates.br>
2021-06-27 22:19:18 -0300

    Comandados

---
+++ src/comandados/Comandados.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comandados;
+
+import java.util.LinkedList;
+
+
+/**
+ *
+ * @author gabri
+ */
+public class Comandados {
+
+    public static LinkedList comandas = new LinkedList();
+    
+    public static void main(String[] args) {
+        Principal p = new Principal();
+        p.setVisible(true);
+              
+    }
+    
+}
---
+++ src/model/Comanda.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author gabri
+ */
+public class Comanda {
+    private int id;
+    private String daCasa;
+    private String burguer;
+    private String kids;
+    private String bacon;
+    private String cheddar; 
+    private String aguaGas;
+    private String agua;
+    private String cocaCola;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDaCasa() {
+        return daCasa;
+    }
+
+    public void setDaCasa(String daCasa) {
+        this.daCasa = daCasa;
+    }
+
+    public String getBurguer() {
+        return burguer;
+    }
+
+    public void setBurguer(String burguer) {
+        this.burguer = burguer;
+    }
+
+    public String getKids() {
+        return kids;
+    }
+
+    public void setKids(String kids) {
+        this.kids = kids;
+    }
+
+    public String getBacon() {
+        return bacon;
+    }
+
+    public void setBacon(String bacon) {
+        this.bacon = bacon;
+    }
+
+    public String getCheddar() {
+        return cheddar;
+    }
+
+    public void setCheddar(String cheddar) {
+        this.cheddar = cheddar;
+    }
+
+    public String getAguaGas() {
+        return aguaGas;
+    }
+
+    public void setAguaGas(String aguaGas) {
+        this.aguaGas = aguaGas;
+    }
+
+    public String getAgua() {
+        return agua;
+    }
+
+    public void setAgua(String agua) {
+        this.agua = agua;
+    }
+
+    public String getCocaCola() {
+        return cocaCola;
+    }
+
+    public void setCocaCola(String cocaCola) {
+        this.cocaCola = cocaCola;
+    }
+
+    
+}
